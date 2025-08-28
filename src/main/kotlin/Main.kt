02692016729fd817d6d4de75fe1f34e00290af5a
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-29 01:22:10 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+package org.example
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
+fun main() {
+   val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations").get()
+
+    var bookQuotes: Elements = doc.select(".bzpNIu")
+
+
+    for (quotes: Element in bookQuotes) {
+        println("${quotes.text()} \n")
+    }
+
+}
